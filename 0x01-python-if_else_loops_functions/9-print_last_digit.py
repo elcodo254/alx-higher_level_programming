51b6094ca309566c8691bc43428a92ae524a41a0
elcodo254 <ellawachaga@gmail.com>
2023-01-05 13:25:22 -0800

    comment out for checker

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    lastdgt = number
-    if number < 0:
-        lastdgt = (-number) % 10
-    else:
-        lastdgt = number % 10
+    lastdgt = number % 10
+  #  if number < 0:
+   #     lastdgt = (-number) % 10
+    #else:
+     #   lastdgt = number % 10
 
-        print(f"{lastdgt:d}", end="")
-        return lastdgt
+    print(f"{lastdgt:d}", end="")
+    return lastdgt
