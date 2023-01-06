522b45f09bef046bdbeba995a07643e14ab35e0e
elcodo254 <ellawachaga@gmail.com>
2023-01-05 16:17:24 -0800

    code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(122, 96, -1):
     if letter % 2 == 0:
-        print(f"{letter:c}", end="")
+        print("{:c}".format(letter), end="")
     else:
-        print(f"{letter - 32:c}", end="")
+        print("{:c}".format(letter - 32), end="")
