8305cd79fddd7d60576a7e906429f288231885ee
elcodo254 <ellawachaga@gmail.com>
2023-01-12 13:41:12 -0800

    code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def hidden_discovery():
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print(i)
-
-
 if __name__ == "__main__":
     import hidden_4
-    hidden_discovery()
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
