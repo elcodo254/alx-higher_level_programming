8ac797532055006fbe181b37a02b1bce593a03e3
elcodo254 <ellawachaga@gmail.com>
2023-01-23 15:57:59 -0800

    checker clearance

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         dval = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(dval))
     return dval
