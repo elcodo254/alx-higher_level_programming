77fa9582cecdd79dbed87fc1f67eaf84db1f443d
elcodo254 <ellawachaga@gmail.com>
2023-01-23 15:24:13 -0800

    clean code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     total = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             total += 1
-    except IndexError:
-        break
-        print("")
-        return total
+        except IndexError:
+            break
+    print("")
+    return total
