710a569a01d78e415dbcd8873dec9c08ad19e0fb
elcodo254 <ellawachaga@gmail.com>
2023-01-24 17:25:57 -0800

    clean code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new Square.
 
         Args:
