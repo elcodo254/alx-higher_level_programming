2f48250d0c2effecdae6fe5ef78b80f3903d7aa9
Angela .W <ellawachaga@gmail.com>
2023-04-13 22:51:48 +0000

    code + test

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+function that returns the addition of 2 integers
+The function must be visible from outside
+The name of the function must be add
+You are not allowed to use var
+*/
+exports.add = (a, b) => a + b;
