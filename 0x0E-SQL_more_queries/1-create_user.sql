e503ee78fa667615bbfbfc75e3b689fe2a664792
elcodo254 <ellawachaga@gmail.com>
2023-04-19 21:45:31 -0700

    code

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
--- script that creates the MySQL server user user_0d_1.
+-- script that creates the MySQL server user user_0d_1 and sets password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
