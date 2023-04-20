74480d4cc2f6d7558ff638c53fc940517ec3c022
elcodo254 <ellawachaga@gmail.com>
2023-04-19 21:54:35 -0700

    ad privileges code

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- script that creates the MySQL server user user_0d_1 and sets password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant full privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
