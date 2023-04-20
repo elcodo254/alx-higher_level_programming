20303a2ddf9a35fa6ef4bf9b2df799df41f291b6
elcodo254 <ellawachaga@gmail.com>
2023-04-20 14:40:24 -0700

    code

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Lists all states from database hbtn_0e_0_usa
+Usage: script takes 3 arguments; username, password and db
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
