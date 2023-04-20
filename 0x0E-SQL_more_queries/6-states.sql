4be444d4288010ddf93078f521c897c14bfc6e01
elcodo254 <ellawachaga@gmail.com>
2023-04-19 23:22:00 -0700

    code

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- create table states
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
