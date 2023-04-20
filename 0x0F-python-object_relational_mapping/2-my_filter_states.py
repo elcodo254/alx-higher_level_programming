2d23cd454acc257854cd38cfb9ad3a3cb8c2c043
elcodo254 <ellawachaga@gmail.com>
2023-04-20 15:24:02 -0700

    pycodestyle debug

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in states table from database hbtn_0e_0_usa
+takes in an argument and displays all values in
+states table from database hbtn_0e_0_usa
 Usage: script takes 4 arguments; username, password, db and state name searched
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id \
+    ASC".format(sys.argv[4]))
     [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
     cur.close()
     db.close()
