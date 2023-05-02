38313b61cad6ae400c88890ec30a7ecc3b2f2367
elcodo254 <ellawachaga@gmail.com>
2023-05-02 12:12:22 -0700

    code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,9 @@ import requests
 
 if __name__ == "__ main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
 
-    req = requests.post(url, data=value)
-    print(req.text)
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
