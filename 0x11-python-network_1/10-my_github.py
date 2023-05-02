443f88688cd99f8998c4e0e49fdb3cbe830ba4aa
elcodo254 <ellawachaga@gmail.com>
2023-05-02 13:43:24 -0700

    code

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("http://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
